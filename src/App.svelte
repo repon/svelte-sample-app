4a4b372c285e1af652db9add75604e910c599d42
repon <repon27@yahoo.co.jp>
2024-11-25 17:17:25 +0900

    画面を整理。時計を作成

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,37 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import Clock from './lib/Clock.svelte'
 </script>
 
-<main>
+<main class="main">
   <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>H.A Svelte Sample Apps</h1>
 
-  <div class="card">
-    <Counter />
+  <div class="item">
+    <h2>シンプルな時計です</h2>
+    <Clock />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
+  .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
   .logo {
     height: 6em;
     padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .item {
+    padding: 2em;
   }
 </style>
---
+++ src/lib/Clock.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  let time = new Date()
+
+
+  const zeroBinding = (num: number):string => num.toString().padStart(2, "0")
+
+  $: hours = zeroBinding(time.getHours());
+	$: minutes = zeroBinding(time.getMinutes());
+	$: seconds = zeroBinding(time.getSeconds());
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      time = new Date()
+    }, 1000);
+    return () => clearInterval(interval)
+  })
+
+</script>
+
+<div class="clock">
+  <time>{hours}:{minutes}:{seconds}</time>
+</div>
+
+<style>
+.clock {
+  font-size: 4em;
+  font-variant-numeric: tabular-nums;
+}
+</style>
