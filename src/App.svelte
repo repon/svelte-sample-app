637efda9aba60fd9ab69d44ba8e5c5306235a157
repon <repon27@yahoo.co.jp>
2024-11-25 17:41:56 +0900

    コードの整理

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
   import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
   import Clock from './lib/Clock.svelte'
 </script>
 
 <main class="main">
-  <div>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>H.A Svelte Sample Apps</h1>
+    <div>
+      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </a>
+    </div>
+    <h1>H.A Svelte Sample Apps</h1>
+    <p class="decription">
+      Svelteで作成したサンプルコンポーネントです。
+      <br />
+      <a href="https://github.com/repon/svelte-sample-app" target="_blank">コードはこちら</a>
+    </p>
 
-  <div class="item">
-    <h2>シンプルな時計です</h2>
-    <Clock />
-  </div>
+    <div class="items">
+      <h2>1. シンプルな時計です</h2>
+      <div class="item">
+        <Clock />
+      </div>
+    </div>
 
 </main>
 
 <style>
+  h1, h2 {
+    margin: 0;
+  }
+
   .main {
     display: flex;
     flex-direction: column;
@@ -31,7 +41,12 @@
     padding: 1.5em;
   }
 
-  .item {
-    padding: 2em;
+  .decription{
+    font-size: 1.2rem
   }
+
+  .items {
+    padding: 2em 0;
+  }
+
 </style>
